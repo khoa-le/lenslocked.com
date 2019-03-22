0c00e5939555e42f7e58b27ea101de5413ed929f
Khoa <khoaln6@gmail.com>
2019-03-22 16:48:54 +0700

    Added Faq page and NotFoundHandler

--- main.go
+++ main.go
@@ -17,9 +17,20 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>My Faq page</h1>")
+}
+func pageNotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadGateway)
+	fmt.Fprint(w, "<h1>We could not find the page you are looking for.</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(pageNotFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
