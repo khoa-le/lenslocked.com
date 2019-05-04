76a4516a12b2f3dd63be4903292e8a46027acad0
Khoa Le <khoa@Khoas-MacBook-Pro.local>
2019-05-04 11:59:07 +0700

    Add new view type

---
+++ views/view.go
@@ -0,0 +1,20 @@
+package view
+
+import "html/template"
+
+func NewView(files ...string) *View {
+	files = append(files, "views/layout/footer.gohtml")
+	
+	t,err := template.ParseFiles(files...)
+	if err != nil{
+		panic(err)
+	}
+
+	return &View{
+		Template: t,
+	}
+}
+
+type View struct {
+	Template *template.Template
+}
