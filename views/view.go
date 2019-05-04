9fe4f118d2ad336a26103bc0140028fb125abf68
Khoa Le <khoa@Khoas-MacBook-Pro.local>
2019-05-04 13:49:03 +0700

    Clean code by globbing template

--- views/view.go
+++ views/view.go
@@ -1,13 +1,18 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir string = "views/layout"
+	TemplateExt string = ".gohtml"
+)
 
 func NewView(layout string, files ...string) *View {
-	files = append(files, 
-		"views/layout/boostrap.gohtml",
-		"views/layout/navbar.gohtml",
-		"views/layout/footer.gohtml",
-	)
+	layoutFiles := layoutFiles()
+	files = append(files, layoutFiles...)
 	
 	t,err := template.ParseFiles(files...)
 	if err != nil{
@@ -24,4 +29,14 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout string
+}
+
+//layoutFiles return a slice of strings representing
+// the layout files used in our application
+func layoutFiles() []string{
+	files, err := filepath.Glob(LayoutDir+ "*" + TemplateExt)
+	if err != nil{
+		panic(err)
+	}
+	return files
 }
