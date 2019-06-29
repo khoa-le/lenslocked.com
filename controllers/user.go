3704728422157605b6cb694e93593ba276eb8a7a
Khoa Le <khoa@Khoas-MacBook-Pro.local>
2019-06-29 10:52:47 +0700

    Parse form with gorilla schema

--- controllers/user.go
+++ controllers/user.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/schema"
 	"lenslocked.com/views"
 )
 func NewUser() *User{
@@ -21,11 +22,21 @@ func (u *User) New(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+type SignupForm struct{
+	Email string `schema:"email"`
+	Password string `schema:"password"`
+}
 // POST /signup
 func (u *User) Create(w http.ResponseWriter, r *http.Request){
 	if err := r.ParseForm(); err !=nil	{
 		panic(err)
 	}
+	var signupForm SignupForm
+	decoder := schema.NewDecoder()
+	if err := decoder.Decode(&signupForm,r.PostForm); err !=nil{
+		panic(err)
+	}
+	fmt.Fprintln(w, signupForm)
 	fmt.Fprintln(w, r.PostForm["email"])
 	fmt.Fprintln(w, r.PostFormValue( "email"))
 	// fmt.Println(w,"This is tempolary response")
