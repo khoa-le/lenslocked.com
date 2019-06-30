838b5613e3ceb93b00b5b226441ee5001c6758b9
Khoa Le <khoa@Khoas-MacBook-Pro.local>
2019-06-30 20:59:26 +0700

    Create user service

--- exp/main.go
+++ exp/main.go
@@ -2,11 +2,9 @@ package main
 
 import(
 	"fmt"
-	"bufio"
-	"os"
-	"strings"
-	"github.com/jinzhu/gorm"
- 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	 _ "github.com/jinzhu/gorm/dialects/postgres"
+	 
+	 "lenslocked.com/models"
 )
 
 const (
@@ -16,84 +14,16 @@ const (
 	dbname = "lenslocked_dev"
 )
 
-type User struct{
-	gorm.Model
-	Name string
-	Email string `gorm:"not null;unique_index"`
-	Orders []Order
-
-}
-
-type Order struct{
-	gorm.Model
-	UserID uint
-	Amount int
-	Description string
-}
-
 func main(){
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable",
 	host, port, user, dbname)
-	db,err := gorm.Open("postgres", psqlInfo)
+	userService,err := models.NewUserService(psqlInfo)
+	defer userService.Close()
 	if err != nil{
 		panic(err)
 	}
-
-	defer db.Close()
-
-	if err := db.DB().Ping(); err !=nil{
-		panic(err)
-	}	
-
-	db.LogMode(true)
-	db.AutoMigrate(&User{},&Order{})
-
-	// name, email := getInfo()
-	// u:=User{
-	// 	Name: name,
-	// 	Email: email,
-	// }
-	// db.Create(&u)
-	// fmt.Println(u)
-
-	// var u1 User
-	// db.First(&u1)
-	// fmt.Println(u1)
-
-	// var users []User
-	// db.Find(&users)
-	// fmt.Println(len(users))
-	// fmt.Println(users)
-
-	var u User
-	if err := db.Preload("Orders").First(&u).Error; err != nil{
-		panic(err)
-	}
-	
-	fmt.Println(u);
-	//createOrder(db,u,1001,"Fake Description Order 1")
+	userService.DestructiveReset()
+	// user,err := userService.ByID(1)
+	// fmt.Println(user)
 
 }
-
-func getInfo() (name, email string){
-	reader :=bufio.NewReader(os.Stdin)
-	fmt.Println("What is your name?")
-	name,_ = reader.ReadString('\n')
-	name = strings.TrimSpace(name)
-
-	fmt.Println("What is your email?")
-	email,_ = reader.ReadString('\n')
-	email = strings.TrimSpace(email)
-	return name,email;
-}
-
-func createOrder(db *gorm.DB,user User,amount int, desc string){
-	err:=db.Create(&Order{
-		UserID: user.ID,
-		Amount: amount,
-		Description: desc,
-	}).Error
-	if err != nil{
-		panic(err)
-	}
-} 
--- models/users.go
+++ models/users.go
@@ -1,9 +1,56 @@
 package models
 
 import (
+	"errors"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+func NewUserService(connectionInfo string) (*UserService, error){
+	db,err := gorm.Open("postgres", connectionInfo)
+	if err != nil{
+		panic(err)
+	}
+
+	return &UserService{
+		db:db,
+	}, nil
+}
+
+type UserService struct{
+	db *gorm.DB
+}
+
+var (
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+//ByID will look up a user with the provided  ID.
+func (us *UserService) ByID(id uint) (*User,error){
+	var user User
+	err := us.db.Where("id=?",id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+//Closes the UserService database connection 
+func (us *UserService) Close() error{
+	return us.db.Close()
+}
+
+//DestructiveReset drops the user table and rebuild it
+func (us *UserService) DestructiveReset(){
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
+
+
 
 type User struct{
 	gorm.Model
