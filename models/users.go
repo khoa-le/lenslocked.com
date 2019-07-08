02a9ef1f64ab8f672156077c0884cc1a66b22074
Khoa Le <khoa@Khoas-MacBook-Pro.local>
2019-07-09 06:58:33 +0700

    Implement remember hmac and brcypt password hash

--- models/users.go
+++ models/users.go
@@ -124,16 +124,18 @@ type userValidator struct{
 //ByRemember will hash the remember token and call the ByRemember
 // on the subsequent on UserDB layer
 func (uv *userValidator) ByRemember(token string) (*User, error){
-	rememberHash := uv.hmac.Hash(token)
-	return uv.UserDB.ByRemember(rememberHash)
+	var user = User{
+		Remember: token,
+	}
+	if err := runUserValidatorFunction(&user, uv.hmacRemember); err!=nil{
+		return nil, err
+	}
+	
+	return uv.UserDB.ByRemember(user.RememberHash)
 }
 
 //Create will create the provided user and backfill data
 func (uv *userValidator) Create(user *User) error{
-	if err:=runUserValidatorFunction(user, uv.bcryptPassword); err!=nil{
-		return err;
-	}
-
 	if user.Remember == ""{
 		token, err  := rand.RememberToken()
 		if err != nil{
@@ -141,16 +143,20 @@ func (uv *userValidator) Create(user *User) error{
 		}
 		user.Remember = token
 	}
-	user.RememberHash = uv.hmac.Hash(user.Remember)
+
+	if err:=runUserValidatorFunction(user, uv.bcryptPassword, uv.hmacRemember); err!=nil{
+		return err;
+	}
 
 	return uv.UserDB.Create(user)
 }
 
 //Update 
 func (uv *userValidator) Update(user *User) error{
-	if user.Remember != ""{
-		user.RememberHash = uv.hmac.Hash(user.Remember)
+	if err:=runUserValidatorFunction(user, uv.hmacRemember); err!=nil{
+		return err;
 	}
+
 	return uv.UserDB.Update(user)
 }
 
@@ -175,6 +181,15 @@ func (uv *userValidator) bcryptPassword(user *User) error{
 	}
 	user.PasswordHash = string(hashedBytes)
 	user.Password = ""
+	return nil
+}
+
+func (uv *userValidator) hmacRemember(user *User) error{
+	if user.Remember == ""{
+		return nil
+	}
+	user.RememberHash = uv.hmac.Hash(user.Remember)
+
 	return nil
 }
 
