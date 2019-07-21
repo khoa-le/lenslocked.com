b3193ac5d0ce41a5ad6b9bf5b9ea5c2c35161a3f
Khoa Le <khoa@Khoas-MacBook-Pro.local>
2019-07-21 23:16:03 +0700

    Add user to request context

--- controllers/galllery.go
+++ controllers/galllery.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"lenslocked.com/context"
 	"lenslocked.com/models"
 	"lenslocked.com/views"
 	"net/http"
@@ -32,8 +33,14 @@ func (g *Gallery) Create(w http.ResponseWriter, r *http.Request) {
 		g.New.Render(w, vd)
 		return
 	}
+
+	user := context.User(r.Context())
+	if user ==nil{
+		http.Redirect(w, r, "/login", http.StatusFound)
+	}
 	gallery := models.Gallery{
 		Title:     form.Title,
+		UserID: user.ID,
 	}
 	if err := g.gs.Create(&gallery); err != nil {
 		vd.SetAlert(err)
